e1365b5b618fb1b5fe41135373b3fddd6d2b000e
JavierCanoy <javier@mabolotech.com>
2022-12-01 06:23:14 +0800

    adding-scrip.js

---
+++ scrip.js
@@ -0,0 +1,124 @@
+// let name = "dung ";
+// function mine() {
+//   document.getElementById("name").innerHTML = " done !  ";
+// }
+// function back() {
+//   document.getElementById("name").innerHTML = "d";
+// }
+
+// let teacher = "javiesdwr";
+// console.log(teacher);
+
+//let use for if you want to change the value in the future !
+// let name = true;
+
+// console.log(typeof name);
+// name = "javier";
+// console.log(typeof name);
+
+// let me;
+// me = "2";
+
+// console.log(me);
+// console.log(typeof me);
+
+// constant cant be change in the fugfure and cant be undifined
+// const birth = 20;
+// const year; need to intializ
+// birth = 21;
+
+//var same as let ! can change value
+// var firstName = "jav2ier";
+// firstName = "jonathan";
+// console.log(firstName);
+
+// operator
+
+// const agejavier = 22;
+// const agejonathan = 51;
+
+// console.log(agejavier / 2);
+// console.log(agejavier * 2, 2 ** 3);
+
+//2 ** 3 means 2 to the power of 3  like 2 * 2 * 2
+
+// const lastName = "bernadas";
+// const lastage = "21";
+// const space = " ";
+
+// console.log(lastName + space + lastage);
+
+//compareson operator (> , > , <= , >= , ==)
+
+// console.log(agejavier > agejonathan);
+// console.log(agejavier >= 22);
+// console.log(typeof agejavier);
+
+// let bmiResultMark;
+// let bmiResultJohn;
+// let markHResult;
+// const markMAss = 78;
+// const markHeigth = 1.69;
+
+// const johnkMAss = 98;
+// const johnHeigth = 1.95;
+
+// bmiResultMark = markMAss / markHeigth ** 2;
+// bmiResultJohn = johnkMAss / johnHeigth ** 2;
+
+// console.log(bmiResultJohn, bmiResultMark);
+// markHResult = bmiResultJohn > bmiResultMark;
+// console.log(markHResult);
+
+//Stirng
+// const firstName = "javier";
+// const lastName = "canoy";
+// const job = "teacher";
+// const byear = 2000;
+// const yearNow = 2022;
+
+// const javier =
+//   "I'm " +
+//   firstName +
+//   " " +
+//   lastName +
+//   "  " +
+//   (yearNow - byear) +
+//   " a" +
+//   " teacher " +
+//   "!";
+
+// console.log(javier);
+// //using back text $
+// const javierNew = ` I'm  ${firstName} , ${lastName} , ${
+//   yearNow - byear
+// } years old and im a ${job}`;
+
+// console.log(javierNew);
+
+//if statment
+
+// const ageJavier = 21;
+// const agejonathan = 21;
+
+// if (agejonathan == ageJavier) {
+//   console.log(`they are the same age ! ...`);
+// } else if (agejonathan > ageJavier) {
+//   console.log(` ${agejonathan} is older than ${ageJavier}  `);
+// } else {
+//   console.log(`  ${ageJavier} is yung than ${agejonathan} `);
+// }
+
+// const age = "12";
+// console.log(typeof age);
+// console.log(Number(age) + 2);
+// console.log(Number(age));
+
+//coertion
+
+console.log("2" + 1);
+console.log("2" * 5);
+
+let num1 = "1" + 2;
+num1 = num1 - 1;
+console.log(num1);
